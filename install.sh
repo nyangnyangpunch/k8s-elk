f14daf3925de53319d52b2e9e656416bfdd7ff9a
leegeunhyeok <dev.ghlee@gmail.com>
2019-08-15 07:00:57 +0900

    UPDATE: shell script

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ echo `java -version`
 
 
 # Download packages
-cd ~
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.0-x86_64.rpm
 wget https://artifacts.elastic.co/downloads/kibana/kibana-7.3.0-x86_64.rpm
 wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.3.0-x86_64.rpm
