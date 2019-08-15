f37ba55e0123cda53ecd56c989c256684761a545
leegeunhyeok <dev.ghlee@gmail.com>
2019-08-15 19:03:02 +0900

    UPDATE: Install script (Need test)

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # k8s-elk
 
+ELK Stack install script for CentOS 7
+
 ```bash
-git clone https://github.com/nyangnyangpunch/k8s-elk.git
-cd k8s-elk
+git clone https://github.com/nyangnyangpunch/k8s-elk.git && cd k8s-elk
 chmod 755 install.sh && ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -12,6 +12,17 @@ wget https://artifacts.elastic.co/downloads/kibana/kibana-7.3.0-x86_64.rpm
 wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.3.0-x86_64.rpm
 wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.3.0-x86_64.rpm
 
+# oc command install
+wget https://github.com/openshift/origin/releases/download/v1.5.1/openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit.tar.gz
+tar –xvf openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit.tar.gz
+mv openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit oc-tool
+cd oc-tool
+export PATH=~/oc-tool:$PATH
+# oc adm policy add-scc-to-user privileged system:serviceaccount:kube-system:metricbeat
+oc patch namespace kube-system -p \
+  '{"metadata": {"annotations": {"openshift.io/node-selector": ""}}}'
+kubectl create -f metricbeat-kubernetes.yaml
+
 # Extract
 # /etc/elasticsearch/jvm.options 
 rpm -ivh elasticsearch-7.3.0-x86_64.rpm
@@ -19,11 +30,8 @@ rpm -ivh kibana-7.3.0-x86_64.rpm
 rpm -ivh filebeat-7.3.0-x86_64.rpm
 rpm -ivh metricbeat-7.3.0-x86_64.rpm
 
-# TODO: metricbeat config
-# /etc/metricbeat/metricbeat.yml
-# /etc/metricbeat/modules.d/kubernetes.yml
 
-# Execute EFK
+# Run
 systemctl start elasticsearch.service
 systemctl start kibana.service
 systemctl start filebeat.service
@@ -34,6 +42,6 @@ metricbeat setup
 systemctl start metricbeat
 
 # Create pod
-kubectl create -f filebeat-kubernetes.yml
+# kubectl create -f filebeat-kubernetes.yml
 
 echo 'Done!'
